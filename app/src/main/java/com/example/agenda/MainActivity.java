d94c716bf416c20bd16ecfa7cdf0ed570c53df1a
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2019-09-04 23:18:30 -0300

    Iniciando a implementação de layout de lista

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -7,6 +7,12 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,10 +21,21 @@ public class MainActivity extends Activity {
         //Toast.makeText(this, "Willian", Toast.LENGTH_LONG).show();
 
         //mansagem apresentada na view por text view
-//        TextView aluno = new TextView(this);
-//        aluno.setText("Willian Oliveira");
-//        setContentView(aluno);
+        //TextView aluno = new TextView(this);
+        //aluno.setText("Willian Oliveira");
+        //setContentView(aluno);
 
+        //chamando a activity_main que apresenta o layout da tela
+        setContentView(R.layout.activity_main);
+
+        //criando array para adicionar lista de alunos
+        List<String> alunos = new ArrayList<>(Arrays.asList("Willian","Amanda","Julia"));
+        TextView primeiroAluno = findViewById(R.id.alunoUm);
+        TextView segundoAluno = findViewById(R.id.alunoDois);
+        TextView terceiroAluno = findViewById(R.id.alunoTres);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
 
     }
 }
