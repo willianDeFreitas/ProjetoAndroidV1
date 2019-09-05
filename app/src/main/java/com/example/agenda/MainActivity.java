931b1bb6f84f052ef0a639b5b6f1317ba84e550e
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2019-09-04 22:54:15 -0300

    duas formas de apresentar mensagens na tela do celular

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -9,6 +11,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //mensagem apresentada na tela
+        //Toast.makeText(this, "Willian", Toast.LENGTH_LONG).show();
+
+        //mansagem apresentada na view por text view
+//        TextView aluno = new TextView(this);
+//        aluno.setText("Willian Oliveira");
+//        setContentView(aluno);
 
 
     }
