941e57a23c71ca735fdfdec2bdb5fa28fd7e0c35
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2019-09-04 23:33:54 -0300

    Utilizando AdapterViews para coleções de views

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,25 +19,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //mensagem apresentada na tela
-        //Toast.makeText(this, "Willian", Toast.LENGTH_LONG).show();
-
-        //mansagem apresentada na view por text view
-        //TextView aluno = new TextView(this);
-        //aluno.setText("Willian Oliveira");
-        //setContentView(aluno);
-
         //chamando a activity_main que apresenta o layout da tela
         setContentView(R.layout.activity_main);
 
-        //criando array para adicionar lista de alunos
-        List<String> alunos = new ArrayList<>(Arrays.asList("Willian","Amanda","Julia"));
-        TextView primeiroAluno = findViewById(R.id.alunoUm);
-        TextView segundoAluno = findViewById(R.id.alunoDois);
-        TextView terceiroAluno = findViewById(R.id.alunoTres);
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+        //utilizando adapter view
+        List<String> alunos = new ArrayList<>(Arrays.asList("Willian", "Amanda", "Julia"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
 
     }
 }
